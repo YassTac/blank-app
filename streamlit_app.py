0bc4b62d6f439832892c0385a765a5c4f73dcc65
YassTac <yassin.tachikart@gmail.com>
2026-03-30 00:42:14 +0200

    Refactor Streamlit app layout and sidebar menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+with st.sidebar :
+    selected = option_menu(menu_title = "Tableau de bord", options = ["Home", "Données Financières", "Données Cliniques", "Contactez nous"], default_index = 0)
+
+if selected == "Home":
+    st.header("Dashboard")
+    
+
+st.title("Dashboard")
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+
