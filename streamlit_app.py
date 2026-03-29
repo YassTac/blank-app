082c16270221ef757d14f020b026eb1ac9d25398
YassTac <yassin.tachikart@gmail.com>
2026-03-30 01:04:32 +0200

    Refactor sidebar menu for better readability

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,29 @@
 import streamlit as st
+import yfinance as yf
 import pandas as pd
 import numpy as np
+import altair as alt
 
 with st.sidebar :
-    selected = option_menu(menu_title = "Tableau de bord", options = ["Home", "Données Financières", "Données Cliniques", "Contactez nous"], default_index = 0)
+    selected = option_menu(
+    menu_title = "Tableau de bord", 
+    options = ["Home", "Données Financières", "Données Cliniques", "Contactez nous"],
+    default_index = 0
+)
 
 if selected == "Home":
     st.header("Dashboard")
+    
+
+
+if selected == "Données financières":
+    st.header("")
+
+if selected == "Données cliniques" :
+    st.header("")
+
+if selected == "Contactez nous" :
+    st.header("")
     
 
 st.title("Dashboard")
